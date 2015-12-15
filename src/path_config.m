356c8c7ed32f5e2e4360a62d0e2eb4b0b89aa17a
Temesghen Kahsai <lememta@gmail.com>
2015-12-15 14:26:34 -0800

    checking

--- src/path_config.m
+++ src/path_config.m
@@ -1,6 +1,6 @@
 function [solver lustrec_bin spacer_build_dir zustre_dir kind2] = path_config()
 %PATH_CONFIG Please specify the paths in this function
-    solver = 'K';
+    solver = 'K'; % change to Z for Zustre and K for Kind2
     lustrec_bin = '/Users/teme/Documents/GitHub/lustrec/bin/lustrec';
     spacer_build_dir = '/Users/teme/Documents/BitBucket/spacer/build/';
     zustre_dir = '/Users/teme/Documents/GitHub/zustre/src/';
