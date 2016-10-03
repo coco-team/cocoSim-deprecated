5bae723d012e2ef87d45a5d5ce115f4605711462
bourbouh <bourbouhh@gmail.com>
2016-10-03 19:02:14 +0200

    fix Z3_LIBRARY init problem

--- src/config.m
+++ src/config.m
@@ -20,26 +20,34 @@
 % Configuration file for the backend solvers
 
 %
- if ~exist('solvers_path', 'var')
+if ~exist('solvers_path', 'var')
     [file_path, ~, ~] = fileparts(mfilename('fullpath'));
     coco_sim_path = fileparts(file_path);
     if ismac
         solvers_path = fullfile(coco_sim_path, 'tools/verifiers/osx/bin/');
-         JKIND =fullfile(coco_sim_path,'tools/verifiers/jkind/jkind');
+        JKIND =fullfile(coco_sim_path,'tools/verifiers/jkind/jkind');
+        Z3Library_path = fullfile(coco_sim_path, 'tools/verifiers/osx/lib/libz3.so');
     elseif isunix
         solvers_path = fullfile(coco_sim_path, 'tools/verifiers/linux/bin/');
-         JKIND =fullfile(solvers_path,'tools/verifiers/jkind/jkind');
+        JKIND =fullfile(solvers_path,'tools/verifiers/jkind/jkind');
+        Z3Library_path = fullfile(coco_sim_path, 'tools/verifiers/linux/lib/libz3.so');
     elseif ispc
-         warndlg('Only JKind can be used', 'CoCoSim backend configuration') 
-        solvers_path = fullfile(coco_sim_path, 'tools/verifiers/'); 
-         JKIND =fullfile(solvers_path,'jkind/jkind');
+        warndlg('Only JKind can be used', 'CoCoSim backend configuration')
+        solvers_path = fullfile(coco_sim_path, 'tools/verifiers/');
+        JKIND =fullfile(solvers_path,'jkind/jkind');
     else
         errordlg('OS not supported yet','CoCoSim backend configuration');
     end
- end
- LUSTREC = fullfile(solvers_path,'lustrec');
- ZUSTRE = fullfile(solvers_path,'zustre');
- Z3 = fullfile(solvers_path,'z3');
- KIND2 = fullfile(solvers_path,'kind2');
- SEAHORN = 'PATH';
+    OldLibPath = getenv('LD_LIBRARY_PATH');
+    if isempty(strfind(OldLibPath,'libz3.so'))
+        setenv('LD_LIBRARY_PATH',Z3Library_path);
+        %to keep the old LD_LIBRARY_PATH use the following (it does not work)
+        %setenv('LD_LIBRARY_PATH',[getenv('LD_LIBRARY_PATH')  Z3Library_path ':']);
+    end
+end
+LUSTREC = fullfile(solvers_path,'lustrec');
+ZUSTRE = fullfile(solvers_path,'zustre');
+Z3 = fullfile(solvers_path,'z3');
+KIND2 = fullfile(solvers_path,'kind2');
+SEAHORN = 'PATH';
 
