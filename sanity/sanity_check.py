52687435a83c0e7375cb323d5f060ca91e27034a
Teme <lememta@gmail.com>
2017-01-24 11:58:12 -0800

    adding sanity

--- sanity/sanity_check.py
+++ sanity/sanity_check.py
@@ -9,17 +9,13 @@ from datetime import datetime, timedelta
 print "Checking for regression testing result ...."
 current_date = time.strftime("%d-%m-%Y")
 current_path = os.path.dirname(os.path.abspath(__file__))
-print current_path
 all_reg = glob.glob(current_path + os.sep + 'regression_result_*')
 
-print all_reg
 for reg in all_reg:
     date_file = (reg.split('regression_result_')[1]).split('@')
     today = datetime.now()  - timedelta(days=2)
     filetime = datetime.fromtimestamp(path.getctime(reg))
-    if filetime < today:
-        print ":("
-    else:
+    if filetime > today:
         print ":) Good Job!!! You run regression today"
         print reg
         exit(0)
