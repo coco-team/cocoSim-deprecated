de70321b2a559c880539b578eb4e6a8f3048ad46
bourbouhh <bourbouhh@gmail.com>
2017-02-13 12:18:56 -0800

    fix parameter issue

--- src/middleEnd/write_global_node.m
+++ src/middleEnd/write_global_node.m
@@ -100,27 +100,27 @@ function [initial_values, variables] = initial_values_initialisation( data, vari
         for i=1:n
             par = Parameters(i);
             try
+                type = sT2fT(par.get('DataType'));
                 par_base = evalin('base',par.Name);
                 if isstruct(par_base)
-                    type = sT2fT(par_base.get('DataType'));
-                    if strcmp(type,'real')
-                        value = sprintf('%f',par_base.Value);
-                    else
-                        if strcmp(type,'bool')
-                            if par_base.Value~=0
-                                value ='true';
-                            else
-                                value = 'false';
-                            end
-                        else
-                            value = num2str(par_base.Value);
-                        end
-                        
-                    end
-                else
-                    value  = num2str(par_base);
-                    type = 'real';
+                    par_base = par_base.Value;
                 end
+                
+                if strcmp(type,'real')
+                    value = sprintf('%f',par_base);
+                else
+                    if strcmp(type,'bool')
+                        if par_base~=0
+                            value ='true';
+                        else
+                            value = 'false';
+                        end
+                    else
+                        value = num2str(par_base);
+                    end
+                    
+                end
+
                 initialisation = [par.Name ' = ',num2str(value), ';\n\n\t'];
                 initial_values = [initial_values, initialisation];
                 variables = [variables, '\t', par.Name, ': ', type , ';\n\n\t'];
