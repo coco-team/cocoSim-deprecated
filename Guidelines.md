2075cfd76cc8f3d0b291b8be17d768220cc565f0
bourbouhh <bourbouhh@gmail.com>
2017-02-16 11:01:51 -0800

    update pp

--- Guidelines.md
+++ Guidelines.md
@@ -1,7 +1,6 @@
 How to be sure your model is well supported by our tool:
 
 * Make sure all Simulink blocks has the same `Sample Time`
-* Make sure your model can be run with `FixedStepDiscrete` before run cocosim.
 * Blocks names should start with a character or underscore
 
 
--- src/pp/default_constants.m
+++ src/pp/default_constants.m
@@ -1,7 +1,7 @@
 % This file contains the default values of different sample times that will
 % be used during the execution of Pre-GAL. The user can modify these values
 % before executing Pre-GAL and they will be used.
-sample = 0.01;
-integrator_sample = 0.01;
-clock_sample = 0.01;
-signal_builder_sample = 0.01;
+sample = -1;
+integrator_sample = -1;
+clock_sample = -1;
+signal_builder_sample = -1;
