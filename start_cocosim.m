86fa423bd39c589e5343a3ed5a2df8bdccaf8751
Teme <lememta@gmail.com>
2017-02-28 18:19:26 -0800

    added a link to start with a simple example

--- start_cocosim.m
+++ start_cocosim.m
@@ -33,6 +33,10 @@ end
 disp('... refreshing customizations')
 addpath(fullfile(cocoSim_path, '.'));
 sl_refresh_customizations;
-clear;
 disp('... CoCoSim is Ready');
+example_model = fullfile(cocoSim_path, 'test', 'properties', 'safe_1.mdl');
+e_message = sprintf('\n\t Click <a href="matlab: open %s">here</a> to start with a simple verification example.', example_model);
+disp('--------------------------')
+disp(e_message);
+clear;
 end
