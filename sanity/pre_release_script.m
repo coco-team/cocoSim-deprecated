7209a56dcd83b3b1eadb9c6a49675850d4189f47
bourbouhh <bourbouhh@gmail.com>
2017-03-09 16:47:04 -0800

    add modifs

--- sanity/pre_release_script.m
+++ sanity/pre_release_script.m
@@ -1,31 +1,43 @@
-function pre_release_script(folder_Path)
+function pre_release_script(folder_Path, version)
 
 if nargin==0
     [file_path, ~, ~] = fileparts(mfilename('fullpath'));
     folder_Path = fileparts(file_path);
 end
+if nargin < 2 
+   version = 'R2014a'; 
+end
 mdl_models = dir(fullfile(folder_Path,'**/*.mdl'));
 slx_models = dir(fullfile(folder_Path,'**/*.slx'));
 models = [mdl_models; slx_models];
 
+
 for i=1:numel(models)
     m = models(i);
-    fprintf('saving model %s in R2014a format\n', m.name);
-    full_path = fullfile(m.folder, m.name);
+    
+    full_path = fullfile(folder_Path, m.name);
     try
-        load_system(full_path);
-        [path, base_name, ext] = fileparts(full_path);
-        target_filename = fullfile(path, strcat(base_name, '_tmp',ext));
-        
-        Simulink.exportToVersion(base_name,target_filename,'R2014b');
-        close_system(full_path,1);
-        delete(full_path);
-        copyfile(target_filename, full_path);
-        delete(target_filename);
-        delete(strcat(full_path,'.r20*'));
-    catch
+        info = Simulink.MDLInfo(full_path);
+        if str2double(info.SimulinkVersion) > 8.3
+            fprintf('saving model %s in %s format\n', m.name, version);
+            load_system(full_path);
+            [path, base_name, ext] = fileparts(full_path);
+            get_param(base_name,'Modelversionformat')
+            target_filename = fullfile(path, strcat(base_name, '_tmp',ext));
+
+            Simulink.exportToVersion(base_name,target_filename,version);
+            close_system(full_path,1);
+            delete(full_path);
+            copyfile(target_filename, full_path);
+            delete(target_filename);
+            delete(strcat(full_path,'.r20*'));
+            display('Done');
+        end
+    catch ME
+        dsplay(ME.getReport())
         continue;
     end
+    
 end
 bdclose('all')
 end
--- src/validate_model.m
+++ src/validate_model.m
@@ -8,14 +8,15 @@ function [valid, validation_compute,lustrec_failed, ...
     lustrec_binary_failed, sim_failed, lus_file_path, ...
     sf2lus_time, nb_actions, Query_time] = validate_model(model_full_path,cocoSim_path, show_models, L, min_max_constraints)
 bdclose('all')
-clc
+% clc
 if ~exist('show_models', 'var')
     show_models = 0;
 elseif show_models
     open(model_full_path);
 end
 if ~exist('cocoSim_path', 'var')
-    cocoSim_path = pwd;
+    file_path = fileparts(mfilename('fullpath'));
+    cocoSim_path = fileparts(file_path);
 end
 
 addpath(fullfile(cocoSim_path,'src/'));
